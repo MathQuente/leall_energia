74dc44cc0dc189b3c1764f9442756593e6bf7a06
Gabriel Nascimento <gabrielnascimentocnsd@gmail.com>
2025-02-22 16:28:57 -0300

    Incorporação de imagem background

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import logoform from '../assets/logo-form.png'
+import worker from '../assets/trabalhador.jpg'
 import { ContactsForm } from "../components/ContactsForm";
 
 export function Contacts(){
     return(
     <>
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
+        <div
+        style={{ backgroundImage: `url(${worker})` }}
+        className="absolute h-screen w-full bg-cover bg-center bg-no-repeat">
+
+    <div className="flex items-center justify-center h-full p-4">
         <div className="bg-white shadow-lg rounded-2xl p-6 max-w-4xl w-full ">
             <h2 className="text-2xl font-semibold text-gray-700 text-left mb-4">Fale Conosco</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -16,6 +21,7 @@ export function Contacts(){
             </div>
            
         </div>
+    </div>
     </div>
     </>
 );
