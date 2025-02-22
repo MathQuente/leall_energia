daabf85c90441650ceccb5d3ffe1929bfdf641d0
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2025-02-22 15:22:51 -0300

    fix: change style of navlink

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,12 +1,21 @@
+import { NavLink as RouterNavLink } from 'react-router-dom'
+
 export function NavLink({ title, href }) {
   return (
     <li>
-      <a
-        href={href}
-        className="text-white font-bold relative after:absolute after:-bottom-1 after:left-0 after:h-[2px] after:w-full after:origin-center after:transform after:scale-x-0 after:bg-white after:transition-transform after:duration-300 after:ease-out hover:after:scale-x-100"
+      <RouterNavLink
+        to={href}
+        className={({ isActive }) =>
+          `text-white font-bold relative after:absolute after:-bottom-1 after:left-0 after:h-[2px] after:w-full after:origin-center after:transform after:bg-white after:transition-transform after:duration-300 after:ease-out 
+          ${
+            isActive
+              ? 'after:scale-x-100'
+              : 'after:scale-x-0 hover:after:scale-x-100'
+          }`
+        }
       >
         {title}
-      </a>
+      </RouterNavLink>
     </li>
   )
 }
