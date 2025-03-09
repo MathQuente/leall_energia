040a2e1799ff15c0e077ba24d0354942cc3b67b0
Gabriel Nascimento <gabrielnascimentocnsd@gmail.com>
2025-03-09 17:18:02 -0300

    Finalizando Services

--- src/components/ServicesGrid.jsx
+++ src/components/ServicesGrid.jsx
@@ -1,17 +1,12 @@
 
 export function ServicesGrid(prop){
     return (
-        <div className="bg-white container mx-auto py-12 px-6">
-            <div className="grid grid-cols-1 md:grid-cols-2 md:gap-8 gap-2 items-center">
-            <div>
-                <h1 className="flex text-3xl font-bold text-gray-950 b-4">{prop.title}</h1>
-                <h2 className="flex text-xl text-gray-600 mt-2">{prop.subtitle}</h2>
-            </div>
-    
-            <div className="flex aspect-1/1 object-cover md:justify-start">
-                <img src={prop.img} alt="Imagem lateral" className="rounded-lg shadow-lg"/>
-            </div>
-            </div>
-        </div>
+    <div className="max-w-md mx-auto py-8 px-6 bg-gray-100 rounded-lg text-center">
+        <div className="flex flex-col items-center">
+            <img src={prop.img} alt="Imagem" className="rounded-lg max-w-40 mb-4" />
+            <h1 className="text-2xl font-bold text-gray-950">{prop.title}</h1>
+            <h2 className="text-xl text-gray-600 mt-2">{prop.subtitle}</h2>
+        </div>
+    </div>
     );
 }
