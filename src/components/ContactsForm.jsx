be3d5f15e5740bb30f653bf6b027d7198ed5e118
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2025-03-09 17:47:46 -0300

    fix: fixing contact page and form

--- src/components/ContactsForm.jsx
+++ src/components/ContactsForm.jsx
@@ -1,59 +1,72 @@
+export function ContactsForm() {
+  return (
+    <>
+      <div className="w-full md:w-1/2 flex flex-col gap-6">
+        <h2 className="text-2xl font-semibold text-[#06b3c2]">
+          Envie sua mensagem por aqui.
+        </h2>
 
-export function ContactsForm(){
-    return(
-        <>
-            <form className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <div>
-                    <label className="block text-sm font-medium text-gray-700">Nome Completo</label>
-                    <input
-                        type="text"
-                        className="mt-1 block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-                        placeholder="Digite seu nome"
-                    />
-                </div>
-                <div>
-                    <label className="block text-sm font-medium text-gray-700">Empresa</label>
-                    <input
-                        type="text"
-                        className="mt-1 block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-                        placeholder="Digite sua Empresa"
-                    />
-                </div>
-                <div>
-                    <label className="block text-sm font-medium text-gray-700">E-mail</label>
-                    <input
-                        type="email"
-                        className="mt-1 block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-                        placeholder="Digite seu e-mail"
-                    />
-                </div>
-                <div>
-                    <label className="block text-sm font-medium text-gray-700">Telefone</label>
-                    <input
-                        type="tel"
-                        pattern="\(\d{2}\) \d{4,5}-\d{4}"
-                        maxLength="15"
-                        className="mt-1 block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-                        placeholder="(DDD) 99999-9999"
-                    />
-                </div>
-                <div className="md:col-span-2">
-                    <label className="block text-sm font-medium text-gray-700">Mensagem</label>
-                    <textarea
-                        type="text"
-                        className="mt-1 p-2 w-full border border-gray-300 shadow-sm rounded-md resize-none h-40 max-h-60"
-                        placeholder="Escreva sua mensagem..."
-                    />
-                </div>
-                <div className="md:col-span-2">
-                    <button
-                        type="submit"
-                        className="w-full bg-indigo-600 text-white p-2 rounded-lg hover:bg-indigo-700 transition"
-                    >
-                        Enviar
-                    </button>
-                </div>
-            </form>
-        </>
-    );
+        <form className="flex flex-col gap-4">
+          <div className="flex flex-col gap-1">
+            <label htmlFor="nome" className="text-gray-700 font-medium">
+              Nome
+            </label>
+            <input
+              type="text"
+              id="nome"
+              name="nome"
+              placeholder="Nome completo"
+              className="bg-white w-full p-3 border border-[#06b3c2] focus:outline-none focus:ring-1 focus:ring-[#06b3c2]"
+            />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <label htmlFor="email" className="text-gray-700 font-medium">
+              E-mail
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Seu e-mail"
+              className="bg-white w-full p-3 border border-[#06b3c2] focus:outline-none focus:ring-1 focus:ring-[#06b3c2]"
+            />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <label htmlFor="telefone" className="text-gray-700 font-medium">
+              Telefone
+            </label>
+            <input
+              type="tel"
+              id="telefone"
+              name="telefone"
+              placeholder="Telefone aqui"
+              className="bg-white w-full p-3 border border-[#06b3c2] focus:outline-none focus:ring-1 focus:ring-[#06b3c2]"
+            />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <label htmlFor="mensagem" className="text-gray-700 font-medium">
+              Mensagem
+            </label>
+            <textarea
+              id="mensagem"
+              name="mensagem"
+              rows="6"
+              placeholder="Seu texto aqui"
+              className="bg-white w-full p-3 border border-[#06b3c2] focus:outline-none focus:ring-1 focus:ring-[#06b3c2]"
+            ></textarea>
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-[#06b3c2] hover:bg-[#059ba9] text-white font-medium py-3 px-4  transition duration-300 mt-2"
+          >
+            Enviar mensagem
+          </button>
+        </form>
+      </div>
+    </>
+  )
 }
